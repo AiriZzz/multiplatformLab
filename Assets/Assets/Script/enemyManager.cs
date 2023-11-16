e34b6d81fc22586e1922bee952c88fc14b758f39
Azreef <zhoreefm@gmail.com>
2023-11-16 18:01:14 +0800

    Updated Gameplay

--- Assets/Assets/Script/enemyManager.cs
+++ Assets/Assets/Script/enemyManager.cs
@@ -9,10 +9,11 @@ public class enemyManager : MonoBehaviour
     [SerializeField] GameObject enemyBarrel;
     [SerializeField] GameObject enemyShip;
     [SerializeField] GameObject enemyIsland;
+    [SerializeField] gameManager GameManager;
     GameObject currentEnemy;
 
     float spawnTimer;
-
+    [SerializeField]float whenToSpawn = 5;
     // Start is called before the first frame update
     void Start()
     {
@@ -24,32 +25,45 @@ public class enemyManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        spawnTimer= spawnTimer + Time.deltaTime;
-
-       // Debug.Log(spawnTimer);
-
-        if(spawnTimer >= 1.5)
+        if (!GameManager.getGameOver())
         {
-            spawnTimer = 0;
+            spawnTimer = spawnTimer + Time.deltaTime;
 
-            int spawnPos = Random.Range(-5, 5);
-            int enemyType = Random.Range(1, 4);
+            // Debug.Log(spawnTimer);
+            if (spawnTimer >= whenToSpawn)
+            {
+                spawnTimer = 0;
 
-            if(enemyType == 1 )
-            {
-                currentEnemy = enemyBarrel;
-            }
-            else if(enemyType == 2 ) 
-            {
-                currentEnemy = enemyShip;
-            }
-            else if(enemyType == 3 ) 
-            {
-                currentEnemy= enemyIsland;
+                if (!(whenToSpawn <= 0.5))
+                {
+                    whenToSpawn = whenToSpawn - 0.1f;
+                }
+
+                int spawnPos = Random.Range(-5, 5);
+                int enemyType = Random.Range(1, 4);
+
+                if (enemyType == 1)
+                {
+                    currentEnemy = enemyBarrel;
+                }
+                else if (enemyType == 2)
+                {
+                    currentEnemy = enemyShip;
+                }
+                else if (enemyType == 3)
+                {
+                    currentEnemy = enemyIsland;
+                }
+
+                Instantiate(currentEnemy, new Vector3(spawnPos, currentEnemy.GetComponent<Renderer>().bounds.size.y / 2, 100), currentEnemy.transform.rotation);
             }
 
-            Instantiate(currentEnemy, new Vector3(spawnPos, currentEnemy.GetComponent<Renderer>().bounds.size.y/2, 100), currentEnemy.transform.rotation);
+
         }
+
+
+
+           
 
     }
 }
--- Assets/Assets/Script/seaScroll.cs
+++ Assets/Assets/Script/seaScroll.cs
@@ -7,7 +7,7 @@ public class seaScroll : MonoBehaviour
 {
     [SerializeField] GameObject sea;
     [SerializeField] GameObject sea2;
-    
+    [SerializeField] gameManager GameManager;
 
     // Start is called before the first frame update
 
@@ -19,18 +19,21 @@ public class seaScroll : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
-        sea.transform.Translate(50*Time.deltaTime,0,0);
-        sea2.transform.Translate(-50* Time.deltaTime, 0, 0);
-
-        if (sea.transform.position.z < -55)
+        if(!GameManager.getGameOver())
         {
-            sea.transform.position = new Vector3(0, 0, sea2.transform.position.z + sea2.GetComponent<Renderer>().bounds.size.z -0.5f);
-        }
+            sea.transform.Translate(50 * Time.deltaTime, 0, 0);
+            sea2.transform.Translate(-50 * Time.deltaTime, 0, 0);
 
-        if (sea2.transform.position.z < -55)
-        {
-            sea2.transform.position = new Vector3(0, 0, sea.transform.position.z + sea.GetComponent<Renderer>().bounds.size.z - 0.5f);
+            if (sea.transform.position.z < -55)
+            {
+                sea.transform.position = new Vector3(0, 0, sea2.transform.position.z + sea2.GetComponent<Renderer>().bounds.size.z - 0.5f);
+            }
+
+            if (sea2.transform.position.z < -55)
+            {
+                sea2.transform.position = new Vector3(0, 0, sea.transform.position.z + sea.GetComponent<Renderer>().bounds.size.z - 0.5f);
+            }
         }
+       
     }
 }
