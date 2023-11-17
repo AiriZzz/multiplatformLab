ea463f344a43aaf158ec1943cec4af517137b425
AiriZzz <121427834+AiriZzz@users.noreply.github.com>
2023-11-17 15:43:21 +0800

    build different

--- Assets/Assets/Script/gameManager.cs
+++ Assets/Assets/Script/gameManager.cs
@@ -48,4 +48,9 @@ public class gameManager : MonoBehaviour
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
+
+    public void returnToMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
 }
